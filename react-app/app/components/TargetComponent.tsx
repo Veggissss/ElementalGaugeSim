4ea1359568403b99d8fd59366b53a1c7bc401e58
Vegard Dæmring <44493351+Veggissss@users.noreply.github.com>
2024-06-14 02:36:27 +0200

    Updated UI

--- react-app/app/components/TargetComponent.tsx
+++ react-app/app/components/TargetComponent.tsx
@@ -3,19 +3,31 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 
 import { ElementName, elementNames } from '../../../simulator/model/Elements/ElementName';
 import { ElementalGauge } from '../../../simulator/model/Elements/ElementalGauge';
 import { ElementType } from '../../../simulator/model/Elements/ElementType';
 import { Target } from '../../../simulator/model/Target';
-import { Reaction } from '../../../simulator/model/Reactions/Reaction';
+import Slider from '@mui/material/Slider';
+import { Box, Grid, Typography } from '@mui/material';
+import { ReactionLog } from '../../../simulator/model/Reactions/ReactionLog';
+
 
 export const TargetComponent = () => {
     const [units, setUnits] = useState(1 as number);
     const [time, setTime] = useState(1 as number);
     const [target, setTarget] = useState(new Target());
-    const [reactions, setReactions] = useState([] as Reaction[]);
+    const [reactions, setReactions] = useState([] as ReactionLog[]);
+
+    const elementColorMap: { [key: string]: string } = {
+        'Pyro': 'red',
+        'Hydro': 'brightblue',
+        'Electro': 'purple',
+        'Cryo': 'SkyBlue',
+        'Dendro': 'green',
+        'Anemo': 'SpringGreen',
+        'Geo': 'Coral',
+    };
 
     const handleAddElement = (element: ElementName) => {
         const newTarget = new Target();
@@ -37,83 +49,133 @@ export const TargetComponent = () => {
     };
 
     const renderElementImages = () => {
-        let elementImages: { [key: string]: string } = {};
-        for (const element of elementNames) {
-            elementImages[element] = `/Element_${element}.png`;
-        }
+        const elementImages: { [key: string]: string } = {};
+        elementNames.forEach((element) => elementImages[element] = `/Element_${element}.png`);
 
         return target.auras.map((aura, index) => (
             <div key={index}>
-                <h3> {aura.element.name} x {aura.gaugeUnits.toFixed(2)}U</h3>
+                <h3>{aura.gaugeUnits.toFixed(2)}U {aura.element.name} Decay: {aura.decayRate.toFixed(2)}U/s</h3>
                 <Image src={elementImages[aura.element.name]} alt={aura.element.name} height={50} width={50} />
             </div>
         ));
     };
 
-    const handleUnitsChange = (event: { target: { value: string; }; }) => {
-        setUnits(parseFloat(event.target.value));
+    const handleUnitsChange = (event: Event, value: number | number[], activeThumb: number) => {
+        if (typeof value === 'number') {
+            setUnits(value);
+        } else if (Array.isArray(value)) {
+            setUnits(value[0]);
+        }
     };
 
-    const handleTimeChange = (event: { target: { value: string; }; }) => {
-        setTime(parseFloat(event.target.value));
+    const handleTimeChange = (event: Event, value: number | number[], activeThumb: number) => {
+        if (typeof value === 'number') {
+            setTime(value);
+        } else if (Array.isArray(value)) {
+            setTime(value[0]);
+        }
     }
 
     const renderReactionImages = () => {
+        const elementImages: { [key: string]: string } = {};
+        elementNames.forEach((element) => elementImages[element] = `/Element_${element}.png`);
+
         return reactions.map((reaction, index) => (
             <div key={index}>
                 <h3> {reaction.name} ({reaction.coefficient}x)</h3>
+                <Box display="flex" alignItems="center">
+                    <Image src={elementImages[reaction.auraElement.element.name]} alt={reaction.auraElement.element.name} height={50} width={50} />
+                    X
+                    <Image src={elementImages[reaction.appliedElement.element.name]} alt={reaction.appliedElement.element.name} height={50} width={50} />
+                </Box>
             </div>
         ));
     };
 
+    const handleRemoveAuras = () => {
+        const newTarget = new Target();
+        Object.assign(newTarget, target); // Copy the current target's properties to the new target
+        newTarget.auras = [];
+
+
+        setReactions([]);
+        setTarget(newTarget);
+    }
+
     return (
         <div>
-            <h1>Elemental Gauge Simulator</h1>
+            <Typography 
+                variant="h1" 
+                gutterBottom 
+                style={{ fontSize: 'calc(1.5em + 2vw)' }} // Adjust the value as needed
+            >
+                Elemental Gauge Simulator
+            </Typography>
+
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box minHeight={250} maxHeight={325}>
+                    <h2>Element Auras:</h2>
+                    <div>{renderElementImages()}</div>
+                </Box>
+
+                <Box minHeight={250} maxHeight={325}>
+                    <h2>Reactions Occurred:</h2>
+                    <div>{renderReactionImages()}</div>
+                </Box>
+            </div>
+            
+
             <div>
                 <h2>Applied Element Unit:</h2>
-                <TextField
-                    type="number"
-                    inputProps={{ step: 0.5, min: 0, max: 10 }}
-                    value={units}
-                    helperText="Gauge Units"
-                    onChange={handleUnitsChange}
-                    variant="outlined"
-                    size="small"
-                    style={{ marginLeft: '4px', backgroundColor: 'DarkGrey' }}
-                />
-                {
-                    elementNames.map((element) => (
-                        <Button key={element} variant="contained" onClick={() =>
-                            handleAddElement(element)}>ADD {element}
-                        </Button>
-                    ))
+                <Box sx={{ width: 300 }}>
+                    <Slider
+                        value={units}
+                        onChange={handleUnitsChange}
+                        min={0}
+                        max={10}
+                        step={0.5}
+                        aria-label="Default"
+                        valueLabelDisplay="auto"
+                        style={{ marginLeft: '8px' }}
+                    />
+                </Box>
+                <Grid container spacing={1}>
+                    {
+                        elementNames.slice(0,7).map((element, index) => (
+                            <Grid item xs={5} sm={4} md={3} lg={0} key={index}>
+                                <Button key={element} variant="contained" style={{ background: elementColorMap[element], justifySelf: "center" }} onClick={() => handleAddElement(element)}>
+                                {units}U {element}
+                                </Button>
+                            </Grid>
+                        ))
+                    }
+                </Grid>
 
-                }
                 <div>
                     <h2>Simulate Time:</h2>
-                    <TextField
-                        type="number"
-                        inputProps={{ step: 0.5, min: 0, max: 10 }}
-                        value={time}
-                        onChange={handleTimeChange}
-                        helperText="Seconds"
-                        variant="outlined"
-                        size="small"
-                        style={{ marginLeft: '4px', backgroundColor: 'DarkGrey' }}
-                    />
+                    <Box sx={{ width: 300 }}>
+                        <Slider
+                            value={time}
+                            onChange={handleTimeChange}
+                            min={0}
+                            max={10}
+                            step={0.5}
+                            aria-label="Default"
+                            valueLabelDisplay="auto"
+                            style={{ marginLeft: '8px' }}
+                        />
+                    </Box>
                     <Button variant="contained" onClick={() =>
-                        handleTimeStep()}>Time Step
+                        handleTimeStep()}>Time Step {time} seconds
                     </Button>
                 </div>
             </div>
-            <div>
-                <h2>Elements Added:</h2>
-                <div>{renderElementImages()}</div>
-            </div>
 
-            <div>
-                <h2>Reactions Occurred:</h2>
-                <div>{renderReactionImages()}</div>
+
+            <div style={{ display: "flex", justifyContent: "center", padding: "2em"}}>
+                <Button variant="outlined" color='error' onClick={() =>
+                    handleRemoveAuras()}>Remove Auras
+                </Button>
             </div>
         </div>
     );
